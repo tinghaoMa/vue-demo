f58839913d7c245aad9c64d969b700f087c9514a
tinghao.ma <tinghao.ma@ele.me>
2019-07-18 22:11:03 +0800

    feat: v-once指令

--- src/components/DynamicComponent.vue
+++ src/components/DynamicComponent.vue
@@ -1,18 +1,21 @@
 <template>
 	<div>
 		<h2>DynamicComponent 动态组件</h2>
-		<component :is="type"></component>
+		<h3>v-once指令保存在内存里面提高性能</h3>
+		<!-- <component :is="type"></component> -->
+		<child-one v-if="type ==='ChildOne'"></child-one>
+		<child-two v-if="type ==='ChildTwo'"></child-two>
 		<button @click="handleBtnClick">Change</button>
 	</div>
 </template>
 
 <script>
 	const ChildOne = {
-		template: '<div>我是动态组件1号</div>'
+		template: '<div v-once>我是动态组件1号</div>'
 	}
 
 	const ChildTwo = {
-		template: '<div>我是动态组件2号</div>'
+		template: '<div v-once>我是动态组件2号</div>'
 	}
 
 	export default {
