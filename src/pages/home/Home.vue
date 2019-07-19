cdcd2f420a32039936de7bb1f5ecc28a9c0d0394
tinghao.ma <tinghao.ma@ele.me>
2019-07-19 14:11:32 +0800

    feat: 网络请求axios

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<home-header></home-header>
+		<div>name = {{name}} age ={{age}}</div>
+	</div>
+
+</template>
+
+<script>
+	import HomeHeader from './components/Header.vue'
+	import axios from 'axios'
+
+	export default {
+		name: 'Home',
+		components: {
+			HomeHeader
+		},
+		data: () => {
+			return {
+				name: 'body content',
+				age:0
+			}
+		},
+		methods: {
+			getHomeInfo() {
+				axios.get('http://rap2api.taobao.org/app/mock/94657/getHomeInfo').then(res => {
+					const {
+						data
+					} = res
+					console.log(data);
+					this.name = data.data.name
+					this.age = data.data.age
+				})
+			}
+		},
+		mounted() {
+			this.getHomeInfo()
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="header">{{name}}</div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeHeader',
+		data: () => {
+			return {
+				name: 'I am Header'
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	.header
+		color #FF0000
+		font-size 20px
+</style>
